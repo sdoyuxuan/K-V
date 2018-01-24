4053da51cc73d6ca12f3d2df8fd0d7e5f3c534ea
sdoyuxuan <513754057@qq.com>
2018-01-24 11:11:26 +0800

    remove all

--- base64.h
+++
@@ -1,7 +0,0 @@
-#ifndef __BASE_64_H__
-#include <string>
-
-std::string base64_encode(char const* , unsigned int len);
-void base64_decode(std::string const& s, std::string& d);
-
-#endif
--- util.h
+++
@@ -1,9 +0,0 @@
-#ifndef KVSTORE_UTIL_UTIL_H_
-#define KVSTORE_UTIL_UTIL_H_
-
-#ifndef UNUSED
-# define UNUSED(x) (void)x
-#endif
-
-#endif  // KVSTORE_UTIL_UTIL_H_
-
